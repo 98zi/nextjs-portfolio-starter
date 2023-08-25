79e9389972d3e7dbc3d184530ccf2ed97d634d9b
lovezsh <shuxhan@163.com>
2023-08-25 09:21:59 +0800

    提交

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: '子舒的博客',
+    site_url: '/',
+    feed_url: '/index.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
